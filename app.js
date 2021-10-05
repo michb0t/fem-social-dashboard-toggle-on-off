03c4f83872824a00a1e999cdf81e68137267cc5b
michb0t <t.pham29@live.com.au>
2021-10-05 12:59:34 +1100

    Updates 5 Oct

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const toggleBtn = document.querySelector(".toggle-switch-inner");
-const toggleSwitch = document.querySelector(".toggle-switch")
+const toggleBtn = document.querySelector(".slider");
+const toggleSwitch = document.querySelector(".toggle-switch");
+const currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : null
 
-toggleBtn.addEventListener("click", function() {
-    this.classList.toggle("active")
-    toggleSwitch.classList.toggle("toggle-dark")
-})
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    if (currentTheme === "dark") {
+        toggleSwitch
+    }
+}
 
+function switchTheme (e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        toggleBtn.classList.toggle("active");
+        toggleSwitch.classList.toggle("toggle-dark");
+        localStorage.setItem("theme", "dark");
+
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        toggleBtn.classList.remove("active");
+        toggleSwitch.classList.remove("toggle-dark");
+        localStorage.setItem("theme", "light");
+
+    }
+}
+
+toggleSwitch.addEventListener("click", switchTheme, false);
 
